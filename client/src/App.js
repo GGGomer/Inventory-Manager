d887194da1e72c86678646006ef8c24aaf7655e7
Kevin Nijmeijer <kevin.nijmeijer@student.hu.nl>
2020-11-30 20:02:13 +0100

    Finished basic frontend functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
-import AppNavbar from './components/AppNavbar';
-import ItemList from './components/ItemList';
+import AppNavbar from "./components/AppNavbar";
+import ItemList from "./components/ItemList";
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
 
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <AppNavbar />
-        <ItemList />
+        <Container>
+          <ItemModal />
+          <ItemList />
+        </Container>
       </div>
     </Provider>
   );
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,22 +1,33 @@
-import { v1 as uuid } from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { v1 as uuid } from "uuid";
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
 
 const initialState = {
-    items: [
-        { id: uuid(), name: 'Sword' },
-        { id: uuid(), name: 'Shield' },
-        { id: uuid(), name: 'Staff' },
-        { id: uuid(), name: 'Armor' }
-    ]
+  items: [
+    { id: uuid(), name: "Sword" },
+    { id: uuid(), name: "Shield" },
+    { id: uuid(), name: "Staff" },
+    { id: uuid(), name: "Armor" },
+  ],
 };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function (state = initialState, action) {
-    switch (action.type) {
-        case GET_ITEMS:
-            return {
-                ...state
-            }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case GET_ITEMS:
+      return {
+        ...state,
+      };
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== action.payload),
+      };
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items],
+      };
+    default:
+      return state;
+  }
 }
