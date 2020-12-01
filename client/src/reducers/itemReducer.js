5f006760e6d20b109fad5b3f0da8f1a35492bd85
Kevin Nijmeijer <kevin.nijmeijer@student.hu.nl>
2020-12-01 10:47:08 +0100

    Hooked up the backend.

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,13 +1,19 @@
-import { v1 as uuid } from "uuid";
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
+// import { v1 as uuid } from "uuid";
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  DELETE_ITEM,
+  ITEMS_LOADING,
+} from "../actions/types";
 
 const initialState = {
   items: [
-    { id: uuid(), name: "Sword" },
-    { id: uuid(), name: "Shield" },
-    { id: uuid(), name: "Staff" },
-    { id: uuid(), name: "Armor" },
+    // { id: uuid(), name: "Sword" },
+    // { id: uuid(), name: "Shield" },
+    // { id: uuid(), name: "Staff" },
+    // { id: uuid(), name: "Armor" },
   ],
+  loading: false,
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -16,16 +22,23 @@ export default function (state = initialState, action) {
     case GET_ITEMS:
       return {
         ...state,
+        items: action.payload,
+        loading: false,
       };
     case DELETE_ITEM:
       return {
         ...state,
-        items: state.items.filter((item) => item.id !== action.payload),
+        items: state.items.filter((item) => item._id !== action.payload),
       };
     case ADD_ITEM:
       return {
         ...state,
         items: [action.payload, ...state.items],
+      };
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true,
       };
     default:
       return state;
